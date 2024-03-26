4dd7549ec8d557b175147bd1832268265b17060b
Jesse <jesse@jessejanssen.nl>
2024-03-26 02:04:06 +0100

    temporary fix i guess

--- argbuilder/public/argparser.py
+++ argbuilder/public/argparser.py
@@ -6,31 +6,36 @@ import sys
 
 from typing import (
     NamedTuple,
-    Self,
+    TypeVar,
 )
 
 
 __all__ = (
     'ArgParser',
+    'parse_args',
 )
 
 
-class ArgParser(NamedTuple):
-    @classmethod
-    def parse_args(
-        cls,
-        *,
-        name: str = MISSING,
-        description: str = 'No description provided',
-        author: str = '69Jesse',
-    ) -> Self:
-        name = name if name is not MISSING else os.path.basename(sys.argv[0]).rsplit('.', 1)[0]
-        builder = Builder.from_named_tuple_cls(
-            named_tuple_cls=cls,
-            name=name,
-            description=description,
-            author=author,
-        )
-        while not builder.finished:
-            builder.iterate()
-        return builder.create_named_tuple()
+ArgParser = NamedTuple
+
+
+NT = TypeVar('NT', bound=ArgParser)
+
+
+def parse_args(
+    cls: type[NT],
+    *,
+    name: str = MISSING,
+    description: str = 'No description provided',
+    author: str = '69Jesse',
+) -> NT:
+    name = name if name is not MISSING else os.path.basename(sys.argv[0]).rsplit('.', 1)[0]
+    builder = Builder.from_named_tuple_cls(
+        named_tuple_cls=cls,
+        name=name,
+        description=description,
+        author=author,
+    )
+    while not builder.finished:
+        builder.iterate()
+    return builder.create_named_tuple()
